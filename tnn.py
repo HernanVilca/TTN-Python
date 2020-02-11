76e2568e3f07f17df4c0a11ca4e504c8c2c91ab9
HernanVilca <hernanmasvi@gmail.com>
2020-02-11 13:39:11 -0500

    save in database posgrests

--- tnn.py
+++ tnn.py
@@ -3,9 +3,24 @@ import sys
 import time
 import ttn
 import base64
+#database
+import psycopg2
+import uuid
 
 app_id = "nodes-tecprec"
 access_key = "ttn-account-v2.I6xWAARi2j9YAI-HCOZbzP-AqubRSqbqpNgnYmvy_Ok"
+
+conn = psycopg2.connect(
+  database="db_tecprec",
+  user="debs",
+  password="123456",
+  host="localhost",
+  port="5435"
+)
+# id_t = uuid.uuid4().hex
+# cur = conn.cursor()
+# cur.execute("INSERT INTO almacen_dato VALUES (%s, %s, %s)", (id_t,'temprepp', '23'))
+# conn.commit()
 
 def uplink_callback(data, client):
   print("*** Received uplink ***")
@@ -17,8 +32,21 @@ def uplink_callback(data, client):
   print("HW serial : ", str(data.hardware_serial))
   print("Frequency : ", str(data.metadata.frequency))
   print("Gateway ID: ", str(data.metadata.gateways[0].gtw_id))
-  tem = data.payload_fields.temperature_2
+  tem = str(data.payload_fields.temperature_2)
   print("Temperatura : ", tem)
+  arra = []
+  arra.append(tem)
+  print ('ssssssss: ', arra)
+  print ('ARRAYS XD: ', arra[0])
+  
+  print ('id', uuid.uuid4().hex)
+  id_tem = uuid.uuid4().hex
+  cur = conn.cursor()
+  tempt = 'temperatura'
+  cur.execute("INSERT INTO almacen_dato VALUES (%s, %s, %s)", (id_tem,tempt, tem))
+  conn.commit()
+  print ('arrays : ', arra_cont)
+
   print("Humedad : ", str(data.payload_fields.relative_humidity_7))
   print()
   print("Raw data : ", str(data.payload_raw))
@@ -34,6 +62,7 @@ handler = ttn.HandlerClient(app_id, access_key)
 # MQTT client
 mqtt_client = handler.data()
 mqtt_client.set_uplink_callback(uplink_callback)
+
 mqtt_client.connect()
 print("Client connected")
 
