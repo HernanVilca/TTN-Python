1c5c6776b10446a9ec9ce2935f3942c228794eca
HernanVilca <hernanmasvi@gmail.com>
2020-02-10 21:30:29 -0500

    ttn demo prueba

--- tnn.py
+++ tnn.py
@@ -1,24 +1,52 @@
+# Imports
+import sys
 import time
 import ttn
+import base64
 
 app_id = "nodes-tecprec"
 access_key = "ttn-account-v2.I6xWAARi2j9YAI-HCOZbzP-AqubRSqbqpNgnYmvy_Ok"
 
 def uplink_callback(data, client):
-  print("Time : ", str(data.metadata.time))
+  print("*** Received uplink ***")
+  # Show uplink message
+  print(str(data))
+  print()
+  print("App_id : ", str(data.app_id))
+  print("Dev_id : ", str(data.dev_id))
+  print("HW serial : ", str(data.hardware_serial))
+  print("Frequency : ", str(data.metadata.frequency))
+  print("Gateway ID: ", str(data.metadata.gateways[0].gtw_id))
+  print("Temperatura : ", str(data.payload_fields.temperature_2))
+  print("Humedad : ", str(data.payload_fields.relative_humidity_7))
+  print()
+  print("Raw data : ", str(data.payload_raw))
+  # Decode base64 message
+  message = base64.b64decode(data.payload_raw)
+  print("Decode B64: " , message.decode("utf-8"))
+  print()
+  
 
+# Define TTN handler
+print("TTN MQTT client started")
 handler = ttn.HandlerClient(app_id, access_key)
-
-# using mqtt client
+# MQTT client
 mqtt_client = handler.data()
 mqtt_client.set_uplink_callback(uplink_callback)
 mqtt_client.connect()
-time.sleep(60)
-mqtt_client.close()
+print("Client connected")
 
-# using application manager client
-app_client =  handler.application()
-my_app = app_client.get()
-print(my_app)
-my_devices = app_client.devices()
-print(my_devices)
+
+# Endless loop
+try:
+# Wait for uplink message
+  while True:
+    time.sleep(60)
+except KeyboardInterrupt:
+# Ctrl+C pressed
+  print()
+  print('Interrupted...')
+  # Graceful exit
+mqtt_client.close()
+print("Client closed")
+sys.exit(0)
